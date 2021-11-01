0d4278749c867f93721bd3cf76320c57bcc9dbec
Temuçin NACAR <nacarct@outlook.com>
2021-11-01 13:14:06 +0300

    Initial commit

---
+++ ClassMethodDemo/CustomerManager.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ClassMethodDemo
+{
+    public class CustomerManager
+    {
+        private readonly List<Customer> _customers = new List<Customer>();
+
+        public void Add(Customer customer)
+        {
+            _customers.Add(customer);
+            Console.WriteLine($"Customer Added : {customer.FirstName} {customer.LastName}");
+        }
+
+        public void Delete(Customer customer)
+        {
+            _customers.Remove(customer);
+            Console.WriteLine($"Customer Deleted : {customer.FirstName} {customer.LastName}");
+        }
+
+        public List<Customer> GetAll()
+        {
+            return _customers;
+        }
+    }
+}
---
+++ ClassMethodDemo/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ClassMethodDemo
+{
+    internal static class Program
+    {
+        private static void Main(string[] args)
+        {
+            CustomerManager customerManager = new CustomerManager();
+
+            Customer customer1 = new Customer()
+            {
+                Id = 1, 
+                IdentityNumber = "12345678901", 
+                FirstName = "Temuçin", 
+                LastName = "Nacar"
+            };
+            
+            
+
+            Customer customer2 = new Customer()
+            {
+                Id = 2,
+                IdentityNumber = "22344618951",
+                FirstName = "Emre",
+                LastName = "Tatar"
+            };
+            
+            Customer customer3 = new Customer()
+            {
+                Id = 3,
+                IdentityNumber = "51354652485",
+                FirstName = "Mehmet",
+                LastName = "Yanar"
+            };
+
+            customerManager.Add(customer1);
+            customerManager.Add(customer2);
+            customerManager.Add(customer3);
+            customerManager.Delete(customer3);
+
+            foreach (var customer in customerManager.GetAll())
+            {
+                Console.WriteLine($"Identity Number : {customer.IdentityNumber} | Id : {customer.Id} | FirstName : {customer.FirstName} | LastName : {customer.LastName}");
+            }
+
+        }
+    }
+}
